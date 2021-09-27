6f5880f337188b008048b3fbcf8172ab9383ec5d
GarimaSChoudhary <90646523+GarimaSChoudhary@users.noreply.github.com>
2021-09-26 23:29:38 -0400

    Add files via upload

---
+++ 002104897_Assignment1/src/model/UserProfile.java
@@ -0,0 +1,179 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author mayan
+ */
+public class UserProfile {
+    
+    private String userName;
+    private String userAddress;
+    private String userDob;
+    private long phoneNo;
+    private long fax;
+    private String userEmail;
+    private String ssn;             
+    private String medicalRecord;
+    private String userHealthPlanNo;
+    private Long bankAccountNo;
+    private String licenseNo;
+    private String vehicleNo;
+    private String deviceSerialNo;
+    private String linkedin;
+    private String ipAddress;
+    private String biometricIdentifier;
+    private String profilePhoto;
+    private String uniqueIdentifier;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserAddress() {
+        return userAddress;
+    }
+
+    public void setUserAddress(String userAddress) {
+        this.userAddress = userAddress;
+    }
+
+    public String getUserDob() {
+        return userDob;
+    }
+
+    public void setUserDob(String userDob) {
+        this.userDob = userDob;
+    }
+
+    public long getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(long phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public long getFax() {
+        return fax;
+    }
+
+    public void setFax(long fax) {
+        this.fax = fax;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getMedicalRecord() {
+        return medicalRecord;
+    }
+
+    public void setMedicalRecord(String medicalRecord) {
+        this.medicalRecord = medicalRecord;
+    }
+
+    public String getUserHealthPlanNo() {
+        return userHealthPlanNo;
+    }
+
+    public void setUserHealthPlanNo(String userHealthPlanNo) {
+        this.userHealthPlanNo = userHealthPlanNo;
+    }
+
+    public Long getBankAccountNo() {
+        return bankAccountNo;
+    }
+
+    public void setBankAccountNo(Long bankAccountNo) {
+        this.bankAccountNo = bankAccountNo;
+    }
+
+    public String getLicenseNo() {
+        return licenseNo;
+    }
+
+    public void setLicenseNo(String licenseNo) {
+        this.licenseNo = licenseNo;
+    }
+
+    public String getVehicleNo() {
+        return vehicleNo;
+    }
+
+    public void setVehicleNo(String vehicleNo) {
+        this.vehicleNo = vehicleNo;
+    }
+
+    public String getDeviceSerialNo() {
+        return deviceSerialNo;
+    }
+
+    public void setDeviceSerialNo(String deviceSerialNo) {
+        this.deviceSerialNo = deviceSerialNo;
+    }
+
+    public String getLinkedin() {
+        return linkedin;
+    }
+
+    public void setLinkedin(String linkedin) {
+        this.linkedin = linkedin;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
+    public String getBiometricIdentifier() {
+        return biometricIdentifier;
+    }
+
+    public void setBiometricIdentifier(String biometricIdentifier) {
+        this.biometricIdentifier = biometricIdentifier;
+    }
+
+    public String getProfilePhoto() {
+        return profilePhoto;
+    }
+
+    public void setProfilePhoto(String profilePhoto) {
+        this.profilePhoto = profilePhoto;
+    }
+
+    public String getUniqueIdentifier() {
+        return uniqueIdentifier;
+    }
+
+    public void setUniqueIdentifier(String uniqueIdentifier) {
+        this.uniqueIdentifier = uniqueIdentifier;
+    }
+    
+}
